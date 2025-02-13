a71aab0c6a62bb5d44dad9c0084e1f5e7359da0d
dhruvild55 <dhruvild@ysoftsolution.com>
2025-02-13 12:40:19 +0530

    remove dist folder

--- dist - Copy/assets/index-DQFB-zv-.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CF-I1X1V.js";const i=({label:n,onClick:o,className:r,disable:s})=>t.jsx("div",{children:t.jsx("button",{onClick:o,className:r,disabled:s,children:n})});export{i as B};
--- dist - Copy/assets/index-Dxsu1xvZ.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CF-I1X1V.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{children:" View Collecter information"})});export{o as default};
